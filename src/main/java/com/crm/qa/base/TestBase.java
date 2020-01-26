dfacddd026d194fdd266e3acaae5a82f42ccafb0
pravins1355 <pravins1355@gmail.com>
2020-01-26 20:41:00 +0530

    updated Test Base class

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,18 +1,30 @@
 package com.crm.qa.base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.crm.qa.util.TestUtil;
 import com.crm.qa.util.WebEventListener;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import com.qa.ExtentReportListener.ExtentManager;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -22,6 +34,9 @@ public class TestBase {
     public static Properties prop;
     public static EventFiringWebDriver e_driver;
     public static WebEventListener eventListener;
+    public static ExtentTest logger;
+    public static ExtentReports extent;
+    public static ExtentHtmlReporter reporter;
 
     public TestBase() {
         try {
@@ -76,4 +91,53 @@ public class TestBase {
         Actions action = new Actions(driver);
         action.moveToElement(ele).click().perform();
     }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        reporter=new ExtentHtmlReporter("./Reports1/learn_automation_"+getCurrentDateTime()+".html");
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws IOException {
+        if(result.getStatus()==ITestResult.FAILURE){
+            logger.info("Reason for Test Failure");
+            logger.error(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(driver)).build());
+            logger.fail("Test Failed", MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(driver)).build());
+        }
+        else if(result.getStatus()==ITestResult.SUCCESS){
+            logger.pass("Test Passed", MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(driver)).build());
+        }
+        else if(result.getStatus()==ITestResult.SKIP){
+            logger.skip("Test Skipped", MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(driver)).build());
+        }
+        driver.quit();
+        extent.flush();
+    }
+
+    @AfterSuite
+    public void aftersuite() {
+
+        //ExtentManager.getReporter().flush();
+    }
+
+    public static String getCurrentDateTime(){
+        DateFormat customFormat=new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");
+        Date currentDate=new Date();
+        return customFormat.format(currentDate);
+    }
+
+    public static String captureScreenshot(WebDriver driver){
+        File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String screenShotPath=System.getProperty("user.dir")+"/screenshots/FreeCRM_"+getCurrentDateTime()+".jpg";
+        try{
+            FileHandler.copy(src,new File(screenShotPath));
+        }
+        catch(IOException e){
+            System.out.println("Unable to capture screenshot "+e.getMessage());
+        }
+        return screenShotPath;
+    }
+
 }
